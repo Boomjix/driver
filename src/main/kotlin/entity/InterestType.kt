5c55e2273c5fa8e26010a68393c9b10149785b1c
Boomjix <bebe1020@hs-karlsruhe.de>
2019-11-02 11:58:06 +0100

    InterestType finished

--- src/main/kotlin/entity/InterestType.kt
+++ src/main/kotlin/entity/InterestType.kt
@@ -1,4 +1,27 @@
 package de.hska.entity
 
-enum class InterestType {
+import com.fasterxml.jackson.annotation.JsonValue
+
+enum class InterestType(val value: String) {
+
+    SPORTS("S"),
+    LECTURE("L"),
+    TRAVEL("T");
+
+
+    @JsonValue
+    override fun toString() = value
+
+    companion object {
+        private val nameCache = HashMap<String, InterestType>().apply{
+            enumValues<InterestType>().forEach{
+                put(it.value, it)
+                put(it.value.toLowerCase(), it)
+                put(it.name, it)
+                put(it.name.toLowerCase(), it)
+            }
+        }
+
+        fun build(value: String?) = nameCache[value]
+    }
 }
