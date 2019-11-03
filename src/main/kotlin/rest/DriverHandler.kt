3445552c96f5c7f56694e0ebe7d84e5cceb26674
Boomjix <bebe1020@hs-karlsruhe.de>
2019-11-03 11:43:26 +0100

    rest/constraints finished

---
+++ src/main/kotlin/rest/DriverHandler.kt
@@ -0,0 +1,4 @@
+package de.hska.rest
+
+class DriverHandler {
+}
---
+++ src/main/kotlin/rest/constraints/DriverConstraintViolation.kt
@@ -0,0 +1,4 @@
+package de.hska.rest.constraints
+
+data class DriverConstraintViolation (val property: String, val message: String?)
+
